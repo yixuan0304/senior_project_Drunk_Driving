6c2daa7aa632716367da817c055f5dae0d68ef88
yixuan0304 <yqiu9610@gmail.com>
2025-08-25 14:57:37 +0800

    First Commit
    Signed-off-by: yixuan0304 <yqiu9610@gmail.com>

---
+++ app/src/main/java/com/example/drunk_driving/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.drunk_driving
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.drunk_driving.ui.theme.Drunk_DrivingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Drunk_DrivingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Drunk_DrivingTheme {
+        Greeting("Android")
+    }
+}
