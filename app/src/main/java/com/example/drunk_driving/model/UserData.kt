80ac19875b06ee6122d8f9ff06e9e0876271672c
yixuan0304 <yqiu9610@gmail.com>
2025-09-19 23:45:06 +0800

    修改UserData
    Signed-off-by: yixuan0304 <yqiu9610@gmail.com>

--- app/src/main/java/com/example/drunk_driving/model/UserData.kt
+++ app/src/main/java/com/example/drunk_driving/model/UserData.kt
@@ -2,7 +2,7 @@ package com.example.drunk_driving.model
 
 data class UserData (
     val email: String,
-    val createdAt: String = "",
+    val createdAt: Long = System.currentTimeMillis(),
     val identity: String = "",
     val loginMethod: String = "",
     val phone: String = "",
