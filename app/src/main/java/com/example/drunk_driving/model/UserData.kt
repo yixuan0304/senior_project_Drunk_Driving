27063be24515bff0373edf1b551b2d8d9664d156
yixuan0304 <yqiu9610@gmail.com>
2025-09-20 16:46:16 +0800

    修改 UserData
    Signed-off-by: yixuan0304 <yqiu9610@gmail.com>

--- app/src/main/java/com/example/drunk_driving/model/UserData.kt
+++ app/src/main/java/com/example/drunk_driving/model/UserData.kt
@@ -1,8 +1,10 @@
 package com.example.drunk_driving.model
 
+import com.google.firebase.Timestamp
+
 data class UserData (
     val email: String,
-    val createdAt: Long = System.currentTimeMillis(),
+    val createdAt: Timestamp = Timestamp.now(),
     val identity: String = "",
     val loginMethod: String = "",
     val phone: String = "",
